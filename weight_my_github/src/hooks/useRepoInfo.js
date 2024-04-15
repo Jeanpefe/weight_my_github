7ee94514c05706f64867e5a52f841008fef3c585
Jeanpefe <jesusangelpequenofernandez@gmail.com>
2024-04-15 22:59:44 +0200

    feat: fetch size from first element of repos

--- weight_my_github/src/hooks/useRepoInfo.js
+++ weight_my_github/src/hooks/useRepoInfo.js
@@ -8,41 +8,19 @@ export default function useRepoInfo() {
     const fetchData = async () => {
         const repoPromise = await fetch(ENDPOINT_URL)
         if (repoPromise.ok) {
-            console.log("Error :(")
             return await repoPromise.json()
         } else {
             console.log("Ya veremos...")
         }
     } 
         
-
     const useGetRepoInfoAndSetState = async () => {
-        console.log(await fetchData())
+        const newSize = await fetchData()
+        if (newSize !== null) {
+            setSize(newSize[0].size)
+            console.log(newSize[0].size)
+        }
     }
 
     return {size, useGetRepoInfoAndSetState}
 }
-
-
-// export default function useRepoInfo() {
-//     const [size, setSize] = useState(null);
-
-//     const fetchRepoInfo = async () => {
-//         try {
-//             const response = await fetch(ENDPOINT_URL);
-//             if (!response.ok) {
-//                 throw new Error('No se pudo obtener la información del repositorio.');
-//             }
-//             const data = await response.json();
-//             setSize(data.length);
-//         } catch (error) {
-//             console.error(error);
-//         }
-//     };
-    
-//     const useGetRepoInfoAndSetState = () => {
-//         fetchRepoInfo();
-//     };
-
-//     return { size, useGetRepoInfoAndSetState };
-// }
